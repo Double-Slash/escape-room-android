d8dd5b20b4aec837e67443840d1773e66804b4b2
seonhyeong <752312@naver.com>
2020-08-05 19:52:28 +0900

    Revert "??"
    This reverts commit bd933a9bcde8beecd0e31358f7081fe8bc8add23.

---
+++ escape/app/src/main/java/com/example/escape/Fragment/MypageFragment.java
@@ -0,0 +1,30 @@
+package com.example.escape.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.escape.R;
+
+public class MypageFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@Nullable LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_mypage , container, false);
+
+        return view;
+
+    }
+
+}
