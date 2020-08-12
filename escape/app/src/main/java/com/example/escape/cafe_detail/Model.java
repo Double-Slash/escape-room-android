c27de674011fa7e3cc0407f325bcbcde65b4e7cb
minj-L <52521457+minj-L@users.noreply.github.com>
2020-08-13 02:38:35 +0900

    Add files via upload
    와이어프레임 9쪽 자바파일입니다

---
+++ escape/app/src/main/java/com/example/escape/cafe_detail/Model.java
@@ -0,0 +1,36 @@
+package com.example.cafe_detail;
+
+public class Model {
+
+    private int image;
+    private String thema;
+    private String cafe;
+
+    public Model(int image, String thema, String cafe) {
+        this.image = image;
+        this.thema = thema;
+        this.cafe = cafe;
+    }
+
+    public int getImage() { return image; }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getThema() {
+        return thema;
+    }
+
+    public void setThema(String thema) {
+        this.thema = thema;
+    }
+
+    public String getCafe() {
+        return cafe;
+    }
+
+    public void setCafe(String cafe) {
+        this.cafe = cafe;
+    }
+}
