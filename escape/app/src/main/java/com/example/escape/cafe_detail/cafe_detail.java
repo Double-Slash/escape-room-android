7889708e137f8d501a4b9531744e3c6eafe3e5dd
minj-L <52521457+minj-L@users.noreply.github.com>
2020-08-14 01:33:31 +0900

    Update cafe_detail.java

--- escape/app/src/main/java/com/example/escape/cafe_detail/cafe_detail.java
+++ escape/app/src/main/java/com/example/escape/cafe_detail/cafe_detail.java
@@ -1,22 +1,35 @@
-package com.example.cafe_detail;
+package com.example.escape.cafe_detail;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.animation.ArgbEvaluator;
+import android.app.FragmentManager;
 import android.media.Rating;
 import android.os.Bundle;
 import android.widget.RatingBar;
 
+import com.example.escape.Fragment.MapFragment;
+import com.example.escape.R;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class cafe_detail extends AppCompatActivity {
+public class cafe_detail extends AppCompatActivity implements OnMapReadyCallback {
 
     ViewPager viewPager;
-    Adapter adapter;
-    List<Model> models;
+    com.example.escape.cafe_detail.Adapter adapter;
+    List<com.example.cafe_detail.Model> models;
     ArgbEvaluator argbEvaluator = new ArgbEvaluator();
+
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +45,12 @@ public class cafe_detail extends AppCompatActivity {
         });
 
         models = new ArrayList<>();
-        models.add(new Model(R.drawable.kiki,"테마명","카페명"));
-        models.add(new Model(R.drawable.kiki,"테마명1","카페명"));
-        models.add(new Model(R.drawable.kiki,"테마명2","카페명"));
-        models.add(new Model(R.drawable.kiki,"테마명3","카페명"));
+        models.add(new com.example.cafe_detail.Model(R.drawable.kiki,"테마명","카페명"));
+        models.add(new com.example.cafe_detail.Model(R.drawable.kiki,"테마명1","카페명"));
+        models.add(new com.example.cafe_detail.Model(R.drawable.kiki,"테마명2","카페명"));
+        models.add(new com.example.cafe_detail.Model(R.drawable.kiki,"테마명3","카페명"));
 
-        adapter = new Adapter(models, this);
+        adapter = new com.example.escape.cafe_detail.Adapter(models, this);
 
         viewPager = findViewById(R.id.viewPager);
         viewPager.setAdapter(adapter);
@@ -59,7 +72,17 @@ public class cafe_detail extends AppCompatActivity {
 
             }
         });
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng location = new LatLng(37.500237, 127.028354);//위치 샘플
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("강남키이스케이프");
+        markerOptions.snippet("방탈출카페");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
 
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,16));
     }
 }
